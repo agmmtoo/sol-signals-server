dda3c67dee179fc7ef210498c0fc72c4e3c6c373
ammo <ammo@noobuntu.lan>
2022-10-06 11:54:17 +0630

    helpful comments

--- controllers/discord.controller.js
+++ controllers/discord.controller.js
@@ -9,7 +9,9 @@ async function base(req, res) {
 
         // get discord token
         const data = await getDiscordToken({ code, state })
-
+        // do smth with data
+        // store data in db or smth
+        // check if user is already authenticated
         res.status(200).json({ success: true, res: data })
 
     } catch (error) {
